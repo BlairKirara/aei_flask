d3744bf16f3f4466dd974c374f5f0f3e294303bf
BlairHaze <126704689+BlairHaze@users.noreply.github.com>
2025-01-17 19:18:36 +0100

    add frequency list

--- frekwencja.py
+++ frekwencja.py
@@ -24,6 +24,23 @@ def bform(clp_instance, slowo):
         print(f"Nie udało się przetworzyć słowa '{slowo}': {e}")
     return slowo
 
+def is_pronoun(clp_instance, word):
+    """Sprawdza, czy słowo jest słowem 'być', nieodmiennym lub zaimkiem na podstawie etykiety CLP."""
+
+    word_bform = bform(clp_instance, word)
+    if word_bform == 'być':
+        return True
+    try:
+        ids = clp_instance.rec(word)
+        if ids:
+            for id in ids:
+                label = clp_instance.label(id)
+                if label.startswith("E") or label.startswith("G"):  # "E" to typ zaimka
+                    return True
+    except Exception as e:
+        print(f"Nie udało się sprawdzić etykiety słowa '{word}': {e}")
+    return False
+
 def generate_frekwencja(folder):
     clp_instance = CLP()
     word_count = defaultdict(int)
@@ -32,13 +49,16 @@ def generate_frekwencja(folder):
 
     for tekst in teksty:
         content = tekst['content']
-        content = re.sub(r'[^\w\s]', ' ', content)  # Wszystko, co nie jest literą lub cyfrą
+        content = re.sub(r'[^\w\s]', ' ', content)
         words = content.split()
 
         for word in words:
+            if is_pronoun(clp_instance, word):
+                continue
             base_form = bform(clp_instance, word)
             word_count[base_form] += 1
 
     sorted_words = sorted(word_count.items(), key=lambda x: x[1], reverse=True)
     return sorted_words
+
 
